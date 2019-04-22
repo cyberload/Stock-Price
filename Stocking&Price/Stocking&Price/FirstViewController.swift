0bec42812506e436d077c25a56d617dd615271dd
lozia <49881518+lozia@users.noreply.github.com>
2019-04-22 15:03:40 -0400

    Add files via upload

---
+++ Stocking&Price/Stocking&Price/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Stocking&Price
+//
+//  Created by 李宗耀 on 4/8/19.
+//  Copyright © 2019 CyberLoad. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Stocking&Price/Stocking&Price/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stocking&Price
+//
+//  Created by 李宗耀 on 4/8/19.
+//  Copyright © 2019 CyberLoad. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
